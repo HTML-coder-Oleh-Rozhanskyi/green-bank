12eec7d9b6b841a7abedc2b940c479b0c1aead92
Rozhanskyi Oleh <oleh.rozhanskiy@gmail.com>
2024-07-29 14:26:29 +0200

    header responsive finish

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-// import { burger } from './components/menu_burger.js';
+import { burger } from './components/menu_burger.js';
 // import { header } from './components/header.js';
 // import { accordion } from './components/accordion.js';
 // import { modal } from './components/modal.js';
