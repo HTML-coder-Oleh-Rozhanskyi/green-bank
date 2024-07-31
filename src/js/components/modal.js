5abc8b36f44d7af7e06192c6faaf9199c4ce722e
Rozhanskyi Oleh <oleh.rozhanskiy@gmail.com>
2024-07-31 20:13:23 +0200

    banner section

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,25 +1,48 @@
 export function modal() {
-	const btns = document.querySelectorAll('.header__button');
+	const btns = document.querySelectorAll('*[data-modal-open-btn]');
 	const modalOverlay = document.querySelector('.modals__overlay');
 	const modalsItem = document.querySelectorAll('.modals__item');
 	const body = document.querySelector('.body');
-	const modalButton = document.querySelector('.modals-btn-close')
+	const modalButtonClose = document.querySelectorAll('.modals-btn-close')
 	const menuWrap = document.querySelector('.menu');
 	const menuBtn = document.querySelector('.burger-btn');
 	const header = document.querySelector('.header');
+	const wrapper = document.querySelector('.wrapper');
+	const focusElement = [
+		'a[href]',
+		'input',
+		'button',
+		'select',
+		'textarea'
+	];
 
 	btns.forEach((el) => {
 		el.addEventListener('click', (e) => {
-			body.classList.add('menu-active')
-			let path = e.currentTarget.getAttribute('data-path');
+
+			let previousActiveElement = e.currentTarget
+
+			let path = e.currentTarget.getAttribute('data-modal-open-btn');
 
 			modalsItem.forEach((el) => {
 				el.classList.remove('modals__item--visible');
 			});
 
-			document.querySelector(`[data-target="${path}"]`).classList.add('modals__item--visible');
+			const targetMOdal = document.querySelector(`[data-modal-window="${path}"]`);
+			wrapper.setAttribute('inert', 'true');
+
 			modalOverlay.classList.add('modals__overlay--visible');
 
+			body.classList.add('menu-active');
+
+			targetMOdal.classList.add('modals__item--visible');
+
+			const focusElementModal = targetMOdal.querySelectorAll(focusElement);
+
+			if (targetMOdal) {
+				setTimeout(() => {
+					focusElementModal[0].focus()
+				}, 200);
+			}
 
 			menuWrap.classList.remove('menu-active');
 			menuBtn.classList.remove('menu-active');
@@ -33,27 +56,38 @@ export function modal() {
 				menuBtn.setAttribute('aria-label', 'open menu');
 				menuBtn.setAttribute('aria-expanded', 'false');
 			}
+			modalButtonClose.forEach((el) => {
+				el.addEventListener('click', () => {
+					previousActiveElement.focus()
+				});
+			});
 		});
+
 	});
 
 	modalOverlay.addEventListener('click', (e) => {
-
 		if (e.target == modalOverlay) {
+			wrapper.removeAttribute('inert', 'true');
 			modalOverlay.classList.remove('modals__overlay--visible');
-			body.classList.remove('menu-active')
+			body.classList.remove('menu-active');
 			modalsItem.forEach((el) => {
 				el.classList.remove('modals__item--visible');
 			});
 		}
 	});
 
-	modalButton.addEventListener('click', (e) => {
-		modalOverlay.classList.remove('modals__overlay--visible');
-		body.classList.remove('menu-active')
-		modalsItem.forEach((el) => {
-			el.classList.remove('modals__item--visible');
+	modalButtonClose.forEach((el) => {
+		el.addEventListener('click', () => {
+			wrapper.removeAttribute('inert', 'true');
+			modalOverlay.classList.remove('modals__overlay--visible');
+			body.classList.remove('menu-active');
+			modalsItem.forEach((el) => {
+				el.classList.remove('modals__item--visible');
+			});
 		});
-
 	});
+
+
 };
+
 modal();
