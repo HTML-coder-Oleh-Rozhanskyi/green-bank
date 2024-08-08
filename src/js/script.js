bc4312fc2bc96b6136c73a99a61efe08f1d0d174
Rozhanskyi Oleh <oleh.rozhanskiy@gmail.com>
2024-08-08 13:30:35 +0200

    quote section responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,9 @@
 import { burger } from './components/menu_burger.js';
 import { header } from './components/header.js';
-import { header } from './components/marquee.js';
+// import { header } from './components/marquee.js';
 // import { accordion } from './components/accordion.js';
 import { modal } from './components/modal.js';
+import { slider } from './components/slider.js';
 
 
 
